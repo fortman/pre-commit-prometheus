c8668e94314a9a8176d363cf6b0a1072e3a2fedf
Ryan Fortman <rfortman@parchment.com>
2019-03-18 14:35:16 -0600

    Removing script language checks and moving to docker_image
    Precommit-Verified: b55b97f4d83fd5e5e096b79dd57f322a93df9ea7a5078cbb270ed3bf5d7c6eec

--- bin/check-rules-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PROMETHEUS_DOCKER_TAG="${1}"
-echo "PROMETHEUS_DOCKER_TAG: ${PROMETHEUS_DOCKER_TAG}"
-shift
-FILES="${*}"
-
-# shellcheck disable=SC2086
-docker run --entrypoint "/bin/promtool" --volume "${PWD}":/repo --workdir "/repo" prom/prometheus:"${PROMETHEUS_DOCKER_TAG}" check rules ${FILES}
