eec886f6f4c36877ac2bc374f64bbebf285e1295
Ryan Fortman <rfortman@parchment.com>
2019-03-18 12:43:39 -0600

    Updating script check to remove update of filenames, and using docker current working directory flag instead.  This will remove absolute file references in the container and use relative instead

--- bin/check-rules-wrapper.sh
+++ bin/check-rules-wrapper.sh
@@ -4,6 +4,5 @@ PROMETHEUS_DOCKER_TAG="${1}"
 echo "PROMETHEUS_DOCKER_TAG: ${PROMETHEUS_DOCKER_TAG}"
 shift
 FILES="${*}"
-CONTAINER_FILES="$( echo ${FILES} | tr " " "\n" | awk ' { print "/repo/"$0 } ' | tr "\n" " " )"
 
-docker run --entrypoint "/bin/promtool" --volume ${PWD}:/repo prom/prometheus:${PROMETHEUS_DOCKER_TAG} check rules ${CONTAINER_FILES}
+docker run --entrypoint "/bin/promtool" --volume ${PWD}:/repo --workdir "/repo" prom/prometheus:${PROMETHEUS_DOCKER_TAG} check rules ${FILES}
