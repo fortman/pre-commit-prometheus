c5632e8401314a1ba0113d8b3183a230b54e78fe
Ryan Fortman <rfortman@parchment.com>
2019-03-18 11:37:20 -0600

    Defining hooks for promtool; check rules and check config

---
+++ bin/check-config-wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PROMETHEUS_DOCKER_TAG="${1}"
+echo "PROMETHEUS_DOCKER_TAG: ${PROMETHEUS_DOCKER_TAG}"
+shift
+FILES="${*}"
+CONTAINER_FILES="$( echo ${FILES} | tr " " "\n" | awk ' { print "/repo/"$0 } ' | tr "\n" " " )"
+
+docker run --entrypoint "/bin/promtool" --volume ${PWD}:/repo prom/prometheus:${PROMETHEUS_DOCKER_TAG} check config ${CONTAINER_FILES}
